9998187882b2aea9e0f249c27058a77bbf2625ea
Darshan Kshetri <126147538+DarshanKGithub@users.noreply.github.com>
2024-08-12 15:24:36 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    function ({ addUtilities }) {
+      addUtilities(
+        {
+          '.scrollbar-hidden': {
+            '-ms-overflow-style': 'none', /* IE and Edge */
+            'scrollbar-width': 'none', /* Firefox */
+          },
+          '.scrollbar-hidden::-webkit-scrollbar': {
+            display: 'none', /* Safari and Chrome */
+          },
+        },
+        ['responsive', 'hover']
+      );
+    },
+  ],
+};
+
+
+
